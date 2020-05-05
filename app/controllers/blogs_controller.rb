f90035fc65b5aba64d64a423ab7f8494358899e5
hiro0911 <evergreen.0911@gmail.com>
2020-05-05 15:30:03 +0900

    [Start] 4-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -17,10 +17,11 @@ class BlogsController < ApplicationController
   def create
   	  blog = Blog.new(blog_params)
   	  blog.save
-  	  redirect_to blogs_path()
+  	  redirect_to blogs_path(blog.id)
   end
 
   def edit
+  	  @blog = Blog.find(params[:id])
   end
   private
   def blog_params
